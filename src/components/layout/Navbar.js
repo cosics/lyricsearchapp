688fe6879e3197d8c98b10f84d31c9b0ed5c58ea
Cezara Luca <cezaraluca@yahoo.com>
2020-04-21 00:18:00 +0300

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-success mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">
+        LyricFinder CeziOficial
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
